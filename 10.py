996db0f0d7480447e1e65d9553320ebf6fc4eaf6
ppvn <pop0vn3322@gmail.com>
2024-02-16 22:52:44 +0700

    add changes

--- 10.py
+++ 10.py
@@ -1,2 +1,12 @@
 number=input('номер рейса: ')
-=input('номер рейса: ')
+nameRU=input('название аваиакомпании(на русском языке): ')
+nameEN=input('название аваиакомпании(на английском языке): ')
+cityRU=input('город прилета (на русском языке): ')
+cityEN=input('город прилета (на английском языке): ')
+print('Заканчивается посадка на рейс',number,nameRU,'до',cityRU)
+print('This is the final boarding call for',nameEN,'flight',number,'to',cityEN)
+
+
+
+
+
